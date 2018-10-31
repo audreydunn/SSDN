8147552d0b0a3e04c236dff423ec3115dfd1ebe4
austindunn7 <dkrux777@gmail.com>
2018-10-30 23:58:24 -0400

    implemented rough draft of transmission states 1-3

--- node.py
+++ node.py
@@ -28,36 +28,72 @@ class StarNode(object):
         self.send_q = deque()
         self.print_q = deque()
 
+        # useful for passing signals :)
+        # Format: (code #, data requested)
+        # code 0 = I need some data
+        # code 1 = Here's the data
+        self.thread_pipe = None
+
     # TODO: write helper methods for vectors
+
+    '''
+    Return thread pipe data
+    '''
+    def get_pipe(self):
+        return self.thread_pipe
+
+    '''
+    Place data into pipe
+    '''
+    def load_pipe(self, code, payload):
+        self.thread_pipe = (code, payload)
+
+    '''
+    Return POC Port
+    '''
+    def get_poc_port(self):
+        return self.poc_port
+
+    '''
+    Return POC Address
+    '''
+    def get_poc_addr(self):
+        return self.poc_addr
 
     '''
     Update/Add a key, value pair to the star map
     '''
-    def update_starmap(key, value):
+    def update_starmap(self, key, value):
         self.star_map[key] = value
 
     '''
     Return value stored in star map
     '''
-    def lookup_starmap(key):
+    def lookup_starmap(self, key):
         return self.star_map[key]
+
+    '''
+    Return current star map
+    '''
+    def get_starmap(self):
+        return self.star_map
 
     '''
     Invert the truth value of hub
     '''
-    def flip_hub():
+    def flip_hub(self):
         self.hub = not self.hub
 
     '''
     Return the identity string
     '''
-    def get_identity():
+    def get_identity(self):
         return self.identity
 
     '''
     Return whether transmission queue is empty
     '''
-    def is_tq_empty():
+    def is_tq_empty(self):
         if len(self.trans_q) > 0:
             return True
         return False
@@ -65,7 +101,7 @@ class StarNode(object):
     '''
     Return whether send queue is empty
     '''
-    def is_sq_empty():
+    def is_sq_empty(self):
         if len(self.send_q) > 0:
             return True
         return False
@@ -73,7 +109,7 @@ class StarNode(object):
     '''
     Return whether print queue is empty
     '''
-    def is_pq_empty():
+    def is_pq_empty(self):
         if len(self.print_q) > 0:
             return True
         return False
@@ -81,37 +117,37 @@ class StarNode(object):
     '''
     Append item to transmission queue
     '''
-    def append_tq(item):
+    def append_tq(self, item):
         self.trans_q.append(item)
 
     '''
     Append item to send queue
     '''
-    def append_sq(item):
+    def append_sq(self, item):
         self.send_q.append(item)
 
     '''
     Append item to print queue
     '''
-    def append_pq(item):
+    def append_pq(self, item):
         self.print_q.append(item)
 
     '''
     Remove and return item from front of transmission queue
     '''
-    def pop_tq():
+    def pop_tq(self):
         return self.trans_q.popleft()
 
     '''
     Remove and return item from front of send queue
     '''
-    def pop_sq():
+    def pop_sq(self):
         return self.send_q.popleft()
 
     '''
     Remove and return item from front of print queue
     '''
-    def pop_pq():
+    def pop_pq(self):
         return self.print_q.popleft()
 
 class Packet(object):
@@ -133,6 +169,7 @@ if __name__ == "__main__":
     max_nodes = sys.argv[5]
     poc_addr = sys.argv[3]
     poc_port = sys.argv[4]
+
     node = StarNode(name, l_addr, l_port, max_nodes, poc_addr, poc_port)
 
     # initialize locks
@@ -140,9 +177,10 @@ if __name__ == "__main__":
     transq_lock = threading.Lock()
     sendq_lock = threading.Lock()
     printq_lock = threading.Lock()
+    pipe_lock = threading.Lock()
 
     # let's make some threads :)
-    args = (node, map_lock, transq_lock, sendq_lock, printq_lock)
+    args = (node, map_lock, transq_lock, sendq_lock, printq_lock, pipe_lock)
     trans_thread = threading.Thread(target=packet_transmission.functional_method, name="trans", args=args)
     recv_thread = threading.Thread(target=packet_retrieval.functional_method, name="recv", args=args)
 
--- packet_retrieval.py
+++ packet_retrieval.py
@@ -1,4 +1,4 @@
 
-def functional_method(node, map_lock, recvq_lock, sendq_lock, printq_lock):
+def functional_method(node, map_lock, recvq_lock, sendq_lock, printq_lock, pipe_lock):
     while(True):
         pass

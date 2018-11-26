c9f25f9a3802a42c6a5db6f2d0f9aea9a70622b3
Nichole <ndeeb3@gatech.edu>
2018-11-25 23:55:22 -0500

    Changed ping interval to include checking History

--- packet_ping.py
+++ packet_ping.py
@@ -5,24 +5,32 @@ import json
 from packets import Packet
 
 
-def core(Star_map, Trans_queue, map_lock, identity, start_pings, End, end_lock):
+def core(Star_map, Trans_queue, History, history_lock, map_lock, identity, start_pings, End, end_lock):
     logger = logging.getLogger('node')
     name, l_addr, l_port = identity.split(":")
     l_port = int(l_port)
     start_pings.wait()
+    counter = 0
     while(True):
         with end_lock:
             if End[0]:
                 break
-        # load queue with low priority packets
-        with map_lock:
-            for node in Star_map:
-                if node != (l_addr, l_port):
-                    payload = json.dumps({
-                        "Map": Star_map.__repr__(),
-                        "Timestamp": datetime.datetime.now().__repr__()
-                    })
-                    packet = Packet(payload, "RTT_REQ", l_addr, l_port, node[0], node[1])
-                    Trans_queue.put((2, packet))
-        # wait for 60 sec
-        time.sleep(20)
+        with history_lock:
+            for curr_packet in History:
+                if (datetime.datetime.now() - curr_packet.get_timestamp()).microseconds >= 500000:
+                    History.remove(curr_packet)
+                    Trans_queue.put((0, curr_packet))
+        counter = counter + 1
+        if counter == 10:  # we start pinging now
+            # load queue with low priority packets
+            with map_lock:
+                for node in Star_map:
+                    if node != (l_addr, l_port):
+                        payload = json.dumps({
+                            "Map": Star_map.__repr__(),
+                            "Timestamp": datetime.datetime.now().__repr__()
+                        })
+                        packet = Packet(payload, "RTT_REQ", l_addr, l_port, node[0], node[1])
+                        Trans_queue.put((2, packet))
+        # wait for 1 sec
+        time.sleep(1)

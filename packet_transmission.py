9d7480d8fb3b1f74ede20d28f01bafaaaed59ac8
Nichole <ndeeb3@gatech.edu>
2018-11-27 18:22:48 -0500

    fixed an oops

--- packet_transmission.py
+++ packet_transmission.py
@@ -21,10 +21,14 @@ def core(Trans_queue, Star_map, map_lock, History, history_lock, End, end_lock):
             type = json_data["Header"]["Type"]
             source_node = (addr, port)
             s.sendto(data.encode('utf-8'), (addr, int(port)))
-            if type != "ACK" and type != "NACK" and type != "RTT_REQ":
-                with history_lock:
-                    History.append(packet)
+
+            if type != "ACK":
                 with map_lock:
                     Star_map[source_node] = [Star_map[source_node][0], Star_map[source_node][1],
                                              (Star_map[source_node][2] + 1), Star_map[source_node][3]]
+
+                if type != "NACK" and type != "RTT_REQ":
+                    with history_lock:
+                        History.append(packet)
+
             logger.info("Sent packet of type {0} to {1}".format(json_data["Header"]["Type"], (addr, port)))
